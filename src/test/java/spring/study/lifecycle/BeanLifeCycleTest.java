e9dae9242146518079c459ce6bcac7ac09cdb8f0
ChoiSangIl <wo566@naver.com>
2022-04-13 15:11:33 +0900

    study: beanLifeCycle

---
+++ src/test/java/spring/study/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package spring.study.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+	@Test
+	public void lifeCycleTest() {
+		ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+		NetworkClient client = ac.getBean(NetworkClient.class);
+		ac.close();
+	}
+	
+	@Configuration
+	static class LifeCycleConfig{
+		
+		//destroyMethod = "close"를 설정하지 않아도 스프링에서 shutdown, close를 자동으로 호출해준다 default AbstractBeanDefinition.INFER_METHOD
+		@Bean(initMethod = "init", destroyMethod = "close")
+		public NetworkClient networkClient() {
+			NetworkClient networkClient = new NetworkClient();
+			networkClient.setUrl("http://hello-spring.dev");
+			return networkClient;
+		}
+	}
+}
